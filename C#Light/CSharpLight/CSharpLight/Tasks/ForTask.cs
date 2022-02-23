cf3412dbdacf54f0aaa197a3a181dfda6e37d6ed
Александр Дрожкин <Santay2009@yandex.ru>
2022-02-23 17:21:16 +0300

    Задача про циклы

---
+++ C#Light/CSharpLight/CSharpLight/Tasks/ForTask.cs
@@ -0,0 +1,26 @@
+﻿using CSharpLight.Tasks.Abstracts;
+
+namespace CSharpLight.Tasks;
+
+public class ForTask : IConsoleTask
+{
+    public void Run()
+    {
+        const uint DefaultCountPrintUserMessage = 10;
+        Console.WriteLine("Введите сообщение");
+        var userMessage = Console.ReadLine();
+        Console.Write("Введите количесво раз отображения сообщения: ");
+        if (!uint.TryParse(Console.ReadLine(), out var countPrintMessage))
+        {
+            countPrintMessage = DefaultCountPrintUserMessage;
+        }
+
+        uint currentCountPrintMessage = default;
+        do
+        {
+            Console.WriteLine(userMessage);
+            currentCountPrintMessage++;
+        } while (currentCountPrintMessage < countPrintMessage);
+
+    }
+}
