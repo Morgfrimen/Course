5a70d24f45178ee1bec85905215bfac3249d9873
Александр Дрожкин <Santay2009@yandex.ru>
2022-02-23 15:27:43 +0300

    Замечание ментора

--- C#Light/CSharpLight/CSharpLight/Tasks/ThreeImageToRow.cs
+++ C#Light/CSharpLight/CSharpLight/Tasks/ThreeImageToRow.cs
@@ -9,7 +9,7 @@ public class ThreeImageToRow : IConsoleTask
         var countUserImages = 52;
         var countPrintImageToRow = 3;
         var countCompletedRows = countUserImages / countPrintImageToRow;
-        var countImagesLeft = countUserImages - 3 * countCompletedRows;
+        var countImagesLeft = countUserImages - countPrintImageToRow * countCompletedRows;
         Console.WriteLine($"Количество полностью заполненых рядов с изображением: {countCompletedRows}");
         Console.WriteLine($"Осталось изображений: {countImagesLeft}");
     }
