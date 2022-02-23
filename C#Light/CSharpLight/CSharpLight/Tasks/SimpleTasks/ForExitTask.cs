f586c0432fa63c4db78de79245baae7449008a5a
Александр Дрожкин <Santay2009@yandex.ru>
2022-02-23 18:28:51 +0300

    Фикс замечаний

--- C#Light/CSharpLight/CSharpLight/Tasks/SimpleTasks/ForExitTask.cs
+++ C#Light/CSharpLight/CSharpLight/Tasks/SimpleTasks/ForExitTask.cs
@@ -9,18 +9,19 @@ public class ForExitTask : IConsoleTask
         const string ExitCommand = "exit";
         Console.WriteLine("Программа с условием выхода");
         var isExitProgram = false;
-        while (!isExitProgram)
+
+        while (isExitProgram is false)
         {
             Console.WriteLine("Выполнение команды...");
             Console.WriteLine(new string('=',50));
             Console.WriteLine("Введите команду");
             var command = Console.ReadLine().ToLower();
+
             if (command is ExitCommand)
             {
                 isExitProgram = true;
                 Console.WriteLine("Конец выполнения программы");
             }
         }
-
     }
 }
