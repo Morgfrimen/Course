80ef20d3e3124c39222a53b08eddead1a2b1e0c5
Александр Дрожкин <Santay2009@yandex.ru>
2022-02-23 15:18:20 +0300

    Маленький фикс второй задачи

--- C#Light/CSharpLight/CSharpLight/Tasks/ThreeImageToRow.cs
+++ C#Light/CSharpLight/CSharpLight/Tasks/ThreeImageToRow.cs
@@ -6,10 +6,10 @@ public class ThreeImageToRow : IConsoleTask
 {
     public void Run()
     {
-        const int countUserImages = 52;
-        const int countPrintImageToRow = 3;
-        const int countCompletedRows = countUserImages / countPrintImageToRow;
-        const int countImagesLeft = countUserImages - 3 * countCompletedRows;
+        var countUserImages = 52;
+        var countPrintImageToRow = 3;
+        var countCompletedRows = countUserImages / countPrintImageToRow;
+        var countImagesLeft = countUserImages - 3 * countCompletedRows;
         Console.WriteLine($"Количество полностью заполненых рядов с изображением: {countCompletedRows}");
         Console.WriteLine($"Осталось изображений: {countImagesLeft}");
     }
