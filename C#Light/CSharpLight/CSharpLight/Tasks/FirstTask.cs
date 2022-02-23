cc9a8168ddd909268892831dfd5ce92452ddfef2
Александр Дрожкин <Santay2009@yandex.ru>
2022-02-23 14:40:16 +0300

    FirstTask

---
+++ C#Light/CSharpLight/CSharpLight/Tasks/FirstTask.cs
@@ -0,0 +1,18 @@
+﻿internal class FirstTask
+{
+    internal void Run()
+    {
+        decimal moneyInWalletIn = 100.55m;
+        int integerValue = 100;
+        double doubleFloatingNumber = 43.55d;
+        float floatingNumber = 34.223f;
+        byte memoryByte = 255;
+        short shortIntegerValue = 256;
+        uint positiveIntegerValue = 299;
+        ushort positiveShortIntegerValue = 2000;
+        string namePerson = "Магнэто";
+        char firstCharInNamePerson = namePerson[0];
+        dynamic objCom = new object(); //Обычно описывается для COM объектов или ActiveX объекты
+        var noDynamicValue = floatingNumber + 23;
+    }
+}
