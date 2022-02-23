dbd2b7a9b61b74ee3ad4cc57b8ba40ec690e9972
Александр Дрожкин <Santay2009@yandex.ru>
2022-02-23 18:26:32 +0300

    Фикс замечаний

--- C#Light/CSharpLight/CSharpLight/Tasks/SimpleTasks/ForTask.cs
+++ C#Light/CSharpLight/CSharpLight/Tasks/SimpleTasks/ForTask.cs
@@ -7,20 +7,23 @@ public class ForTask : IConsoleTask
     public void RunTask()
     {
         const uint DefaultCountPrintUserMessage = 10;
+
         Console.WriteLine("Введите сообщение");
         var userMessage = Console.ReadLine();
         Console.Write("Введите количесво раз отображения сообщения: ");
-        if (!uint.TryParse(Console.ReadLine(), out var countPrintMessage))
+
+        if (uint.TryParse(Console.ReadLine(),out var countPrintMessage) is false)
         {
             countPrintMessage = DefaultCountPrintUserMessage;
         }
 
         uint currentCountPrintMessage = default;
+
         do
         {
             Console.WriteLine(userMessage);
             currentCountPrintMessage++;
-        } while (currentCountPrintMessage < countPrintMessage);
-
+        }
+        while (currentCountPrintMessage < countPrintMessage);
     }
 }
